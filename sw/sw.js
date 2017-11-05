89b5a910787e6e5880e22a4009d564880d57356a
jiharAkakom <jihar.akakom14@gmail.com>
2017-11-05 19:56:39 +0700

    My CV

--- sw/sw.js
+++ sw/sw.js
@@ -9,18 +9,8 @@ var filesToCache = [
   '/img/golang.png',
   '/img/grpc.png',
   '/img/laravel.png',
-  '/img/profile.png'
+  '/img/profile.png',
 ];
-
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
