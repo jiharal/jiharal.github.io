3aff30bfc465986854a2b2c600c72805e2df0e1c
jiharal <jihar.akakom14@gmail.com>
2019-12-17 04:51:00 +0700

    rebuilding site Tue Dec 17 04:51:00 WIB 2019

--- sw/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('sw/sw.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
